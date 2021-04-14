f6d56db23d86a7864f950da3ef2e07fba5b63643
armando122 <armandoramirezg@ciencias.unam.mx>
2021-04-14 17:15:22 -0500

    Iniciando pruebas

---
+++ Listas/listas/src/test/scala/proyectos/ListasTest.scala
@@ -0,0 +1,60 @@
+package proyectos
+
+import org.scalatest.funsuite.AnyFunSuite
+import proyectos.Listas
+
+class ListasTest extends AnyFunSuite {
+
+  test("Listas.agrega") {
+    assert(Listas.agrega(3) == l)
+  }
+
+  test("Listas.elimina") {}
+
+  test("Listas.contiene") {}
+
+  test("Listas.esVacia") {}
+
+  test("Listas.elementos") {}
+
+  test("Listas.limpia") {}
+
+  test("Listas.toString") {}
+
+  test("Listas.equals") {}
+
+  test("Listas.longitud") {}
+
+  test("Listas.agregaFinal") {}
+
+  test("Listas.agregaInicio") {}
+
+  test("Listas.inserta") {}
+
+  test("Listas.eliminaPrimero") {}
+
+  test("Listas.eliminaUltimo") {}
+
+  test("Listas.primero") {}
+
+  test("Listas.ultimo") {}
+
+  test("Listas.obtener") {}
+
+  test("Listas.copia") {}
+
+  test("Listas.reversa") {}
+
+  test("Listas.indiceDe") {}
+
+  test("Listas.hasNext") {}
+
+  test("Listas.next") {}
+
+  test("Listas.hasPrevious") {}
+
+  test("Listas.start") {}
+
+  test("Listas.end") {}
+
+}
